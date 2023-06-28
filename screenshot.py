da692daad70919c7715eaf59b12764b88e480165
Wata <forgotoffcom@gmail.com>
2023-06-28 15:22:01 +0800

    Implemented screenshot

---
+++ screenshot.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+def capture_window(window_name):
+    # Get window ID
+    command = f"xdotool search --name '{window_name}'"
+    output = subprocess.check_output(command, shell=True).decode("utf-8").strip()
+    window_id = int(output)
+
+    output_file = "browser_screenshot.png"
+    command = f"import -window {window_id} {output_file}"
+    subprocess.run(command, shell=True)
+
+
+if __name__ == "__main__":
+    capture_window("Play Tetris")
